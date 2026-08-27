d539fc66510d574543f423c28ae7c9c2b1e1d56a
Prabhashana Jayarathne <sumuduprabashana36@gmail.com>
2026-08-27 23:13:55 +0530

    Initial Commit

---
+++ src/main/java/lk/megasupply/esms_backend/EsmsBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.megasupply.esms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/megasupply/esms_backend/EsmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.megasupply.esms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
